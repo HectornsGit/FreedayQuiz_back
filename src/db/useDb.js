24f5cc515a3875ceca6c95a1caf5f400fc33c12d
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-05-06 19:25:47 +0200

    .env and .env.example files. Script initDb in package.json. Install mysql2. Create files dropDb, initDb, useDb and getPoll

---
+++ src/db/useDb.js
@@ -0,0 +1,12 @@
+import pool from './getPool.js';
+
+const useDb = async () => {
+  try {
+    await pool.query(`USE ${process.env.NAME_DB};`);
+  } catch (error) {
+    error.message = `La base de datos ${process.env.NAME_DB} no existe. Ejecuta el initDb para iniciarla`;
+    process.exit(1);
+  }
+};
+
+export default useDb;

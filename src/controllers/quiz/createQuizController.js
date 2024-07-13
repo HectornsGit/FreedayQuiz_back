99243dcae9e50238972e0473faea822e9adf7e92
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-07-13 18:35:36 +0200

    UUID as a quiz id

--- src/controllers/quiz/createQuizController.js
+++ src/controllers/quiz/createQuizController.js
@@ -1,54 +1,54 @@
-import { createQuiz } from '../../models/quiz/index.js';
-import { validationSchemaQuiz } from '../../utils/index.js';
-import qr from 'qrcode';
-import { generateCode } from '../../utils/index.js';
+import { createQuiz } from '../../models/quiz/index.js'
+import { validationSchemaQuiz } from '../../utils/index.js'
+import qr from 'qrcode'
+import { generateCode } from '../../utils/index.js'
 
 const createQuizController = async (req, res, next) => {
-  try {
-    const { title, description } = req.body;
-    const loggedUserId = req.auth?.id;
+    try {
+        const { title, description } = req.body
+        const loggedUserId = req.auth?.id
 
-    //Validación con Joi:
-    const { error } = validationSchemaQuiz.validate(req.body);
-    if (error) {
-      error.message = error.details[0].message;
-      throw error;
+        //Validación con Joi:
+        const { error } = validationSchemaQuiz.validate(req.body)
+        if (error) {
+            error.message = error.details[0].message
+            throw error
+        }
+
+        //Generación de código de acceso aleatorio:
+        const accessCode = generateCode(4)
+
+        //----------------------------
+        const insertId = await createQuiz(
+            title,
+            description,
+            loggedUserId,
+            accessCode
+        )
+        console.log('id', insertId[0].id)
+        //Creación de código QR:
+        const quizURL = await qr.toDataURL(
+            `${process.env.FRONT_URL}/quiz/${insertId[0].id}`
+        )
+
+        res.send({
+            status: 'ok',
+            message: 'Has creado un quiz!👌',
+            data: {
+                id: insertId[0].id,
+                title,
+                description,
+                accessCode,
+                ownerId: loggedUserId,
+                qrCode: {
+                    quizId: insertId[0].id,
+                    url: quizURL,
+                },
+            },
+        })
+    } catch (error) {
+        next(error)
     }
+}
 
-    //Generación de código de acceso aleatorio:
-    const accessCode = generateCode(4);
-
-    //----------------------------
-    const insertId = await createQuiz(
-      title,
-      description,
-      loggedUserId,
-      accessCode
-    );
-
-    //Creación de código QR:
-    const quizURL = await qr.toDataURL(
-      `${process.env.FRONT_URL}/quiz/${insertId}`
-    );
-
-    res.send({
-      status: 'ok',
-      message: 'Has creado un quiz!👌',
-      data: {
-        id: insertId,
-        title,
-        description,
-        accessCode,
-        ownerId: loggedUserId,
-        qrCode: {
-          quizId: insertId,
-          url: quizURL,
-        },
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-export default createQuizController;
+export default createQuizController
--- src/models/quiz/createQuiz.js
+++ src/models/quiz/createQuiz.js
@@ -1,12 +1,16 @@
-import pool from '../../db/getPool.js';
-import useDb from '../../db/useDb.js';
+import pool from '../../db/getPool.js'
+import useDb from '../../db/useDb.js'
 const createQuiz = async (title, description, loggedUserId, accessCode) => {
-  await useDb();
-  const [{ insertId }] = await pool.query(
-    'INSERT INTO quizzes ( title, description, owner_id, access_code)VALUES(?,?,?,?)',
-    [title, description, loggedUserId, accessCode]
-  );
-  return insertId;
-};
+    await useDb()
+    await pool.query(
+        'INSERT INTO quizzes ( title, description, owner_id, access_code)VALUES(?,?,?,?)',
+        [title, description, loggedUserId, accessCode]
+    )
+    const [id] = await pool.query(
+        `SELECT id from quizzes WHERE access_code = (?)`,
+        [accessCode]
+    )
+    return id
+}
 
-export default createQuiz;
+export default createQuiz

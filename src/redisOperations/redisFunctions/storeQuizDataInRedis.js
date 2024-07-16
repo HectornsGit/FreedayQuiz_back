b37cbc5313c1efb221c867c9698aee4953a04723
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-07-16 21:26:09 +0200

    Dropdown list for questions, done

--- src/redisOperations/redisFunctions/storeQuizDataInRedis.js
+++ src/redisOperations/redisFunctions/storeQuizDataInRedis.js
@@ -17,7 +17,17 @@ export async function storeQuizDataInRedis(quizData, socket) {
             'number_of_questions',
             quizData.questions.length
         )
+        //Guardo la lista de preguntas para mostrar en un desplegable desde el front:
+        const listOfQuestions = quizData.questions.map((question) => {
+            return { title: question.question, number: question.questionNumber }
+        })
+        await redisClient.hSet(
+            quizKey,
+            'list_of_questions',
+            JSON.stringify(listOfQuestions)
+        )
 
+        // Como tercer paso, guardo cada pregunta en Redis como un string en un key con su número de pregunta:
         // Guardo las preguntas de forma separada, con un key para cada una, para la fácil obtención y edición de cada una:
         quizData.questions.forEach(async (question) => {
             const questionKey = `quiz:${quizData.id}:question:${question.questionNumber}`

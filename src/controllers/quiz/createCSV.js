a11f4d2189ee3b17e407f749b463fcc1bd71805f
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-08-10 20:33:27 +0200

    Ruta y controlador para crear archivo CSV y mandarlo al front

--- src/controllers/quiz/createCSV.js
+++ src/controllers/quiz/createCSV.js
@@ -4,12 +4,19 @@ import { Parser } from '@json2csv/plainjs'
 const createCSV = async (req, res, next) => {
     try {
         const playersData = req.body
-        const parser = new Parser()
-        const csv = parser.parse(playersData)
+        const dataToConvert = playersData.map((player) => {
+            return {
+                Jugador: player.name,
+                Puntos: player.totalScore,
+                Racha: player.streak,
+            }
+        })
+        const parser = new Parser({ fields: ['Jugador', 'Puntos', 'Racha'] })
+        const csv = parser.parse(dataToConvert)
         if (!csv) {
             generateError('No se pudieron convertir los datos', 400)
         }
-        res.send({ message: 'Datos convertidos a .csv', csv: csv })
+        res.send(csv)
     } catch (err) {
         next(err)
     }

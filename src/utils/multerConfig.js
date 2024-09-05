84faa0ba4485ab406fa9fa92d984925721aff3b0
HectornsGit <hector.novoa.segade@gmail.com>
2024-09-05 20:12:27 +0200

    chore: adds logs for debugging purposes

--- src/utils/multerConfig.js
+++ src/utils/multerConfig.js
@@ -3,29 +3,31 @@ import path from 'path'
 
 //Guardar en disco:
 const storage = multer.diskStorage({
-    destination: function (_req, _file, cb) {
-        cb(null, './src/uploads/') // Directorio donde se guardarán las fotos de perfil
-    },
-    filename: function (_req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname))
-    },
+  destination: function (_req, _file, cb) {
+    cb(null, './src/uploads/') // Directorio donde se guardarán las fotos de perfil
+  },
+  filename: function (_req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname))
+  },
 })
+
+console.log('Multer ', storage)
 //Guardar en buffer:
 const bufferStorage = multer.memoryStorage()
 
 //Validar que sea un archivo de imagen:
 const fileFilter = (_req, file, callback) => {
-    // Verificar el tipo de archivo permitido
-    const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
-    if (allowedMimeTypes.includes(file.mimetype)) {
-        callback(null, true) // Aceptar el archivo
-    } else {
-        callback(new Error('Tipo de archivo no permitido'), false) // Rechazar el archivo
-    }
+  // Verificar el tipo de archivo permitido
+  const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
+  if (allowedMimeTypes.includes(file.mimetype)) {
+    callback(null, true) // Aceptar el archivo
+  } else {
+    callback(new Error('Tipo de archivo no permitido'), false) // Rechazar el archivo
+  }
 }
 //Validar el tamaño de imagen:
 const limits = {
-    fileSize: 3 * 1024 * 1024, // Maximo archivos de 3 megabytes.
+  fileSize: 3 * 1024 * 1024, // Maximo archivos de 3 megabytes.
 }
 
 export { storage, limits, fileFilter, bufferStorage }

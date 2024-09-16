c59898d18e123e145c89114694d71d1c2d097309
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-09-16 17:17:31 +0200

    Añadir imagen aleatoria cuando se registra un usuario

--- src/utils/index.js
+++ src/utils/index.js
@@ -14,6 +14,7 @@ import endQuizUtil from './endQuizUtil.js'
 import sendMailUtil from './sendMailUtil.js'
 import resizeImages from './resizeImages.js'
 import cloudinary from './cloudinaryConfig.js'
+import generateRandomAvatar from './generateRandomAvatar.js'
 
 export {
     generateError,
@@ -33,4 +34,5 @@ export {
     sendMailUtil,
     resizeImages,
     cloudinary,
+    generateRandomAvatar,
 }
